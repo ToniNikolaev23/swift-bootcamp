7d5a3bdd4f28ee52784101750c27fbd355c80751
Toni Stoyanov <toni.n.stoyanov@icloud.com>
2024-11-12 20:32:27 +0200

    Init Swift bootcamp

---
+++ SwiftBootcamp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftBootcamp
+//
+//  Created by Toni Stoyanov on 12.11.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftBootcamp/SwiftBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftBootcampApp.swift
+//  SwiftBootcamp
+//
+//  Created by Toni Stoyanov on 12.11.24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
