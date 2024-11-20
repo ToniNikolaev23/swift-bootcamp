58ef03391906ba978b8b71b8731a7d1a5574edf5
Toni Stoyanov <toni.n.stoyanov@icloud.com>
2024-11-20 10:51:31 +0200

    Ternary

--- SwiftBootcamp/TernaryBootcamp.swift
+++ SwiftBootcamp/TernaryBootcamp.swift
@@ -8,8 +8,19 @@
 import SwiftUI
 
 struct TernaryBootcamp: View {
+    @State var isStartingState: Bool = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Button(action: {
+                isStartingState.toggle()
+            }, label: {
+                Text("Button")
+            })
+            
+            RoundedRectangle(cornerRadius: isStartingState ? 25.0 : 0)
+                .fill(isStartingState ? Color.red : Color.blue)
+                .frame(width: isStartingState ? 200 : 50, height: 100)
+        }
     }
 }
 
